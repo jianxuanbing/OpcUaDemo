e2ae0a461808a8bc8cce6a36529f1e9a8cc23566
多态 <xufenghui@126.com>
2019-03-12 10:47:20 +0800

    opc ua sample.

---
+++ MicrosoftOpcUa.Client/MicrosoftOpcUa.Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using MicrosoftOpcUa.Client.Subscribers;
+using MicrosoftOpcUa.Client.Utility;
+
+using Opc.Ua;
+using Opc.Ua.Client;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicrosoftOpcUa.Client
+{
+    class Program
+    {
+        private static OpcUaClient client;
+        static void Main(string[] args)
+        {
+            Console.Title = "HuaWei OPC_UA Client";
+            Task result = ConnectAsync();
+            result.ContinueWith((task) =>
+            {
+                Console.WriteLine("Connect to  opc.tcp://localhost:62567 success!");
+                SubscriberManager.OpcUaClient = client;
+                SubscriberManager.RegistSubscriber();
+             
+            });
+            Console.WriteLine("Please enter any key to quit!");
+            Console.ReadLine();
+            Disconnect();
+        }
+
+        private static async Task ConnectAsync()
+        {
+            client = new OpcUaClient
+            {
+                UserIdentity = new UserIdentity(new AnonymousIdentityToken())
+            };
+            await client.ConnectServer("opc.tcp://localhost:62567");
+        }
+
+        private static void Disconnect()
+        {
+            client.Disconnect();
+        }
+    }
+}
